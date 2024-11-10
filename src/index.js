400ca6c17f07f718289eca9300608428a94ea97a
Denelia <janinedenelia7@gmail.com>
2024-11-10 16:22:54 +0200

    added apiKey and apiUrl with functions

--- src/index.js
+++ src/index.js
@@ -1,9 +1,24 @@
+function refreshWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function searchCity(city) {
+  let apiKey = "ba30d32cb59f4of366560at60dd0fa87";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+  axios.get(apiUrl).then(refreshWeather);
+}
+
 function search(event) {
   event.preventDefault();
-  let searchInputElement = document.querySelector("#search-input");
-  let cityElement = document.querySelector("#current-city");
-  cityElement.innerHTML = searchInputElement.value;
+  let searchInput = document.querySelector("#search-input");
+  searchCity(searchInput.value);
 }
 
 let searchEngineElement = document.querySelector("#search-engine");
 searchEngineElement.addEventListener("submit", search);
+
+searchCity("Durban");
