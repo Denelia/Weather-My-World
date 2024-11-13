1f0f3fb31f5c3d88eb612ea6ced01a9e31126050
Denelia <janinedenelia7@gmail.com>
2024-11-13 23:40:34 +0200

    added html through javascript for weather forecast

--- src/index.js
+++ src/index.js
@@ -30,6 +30,28 @@ function search(event) {
   let searchInput = document.querySelector("#search-input");
   searchCity(searchInput.value);
 }
+function displayForecast() {
+  let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
+  let forecastHtml = "";
+  days.forEach(function (day) {
+    forecastHtml =
+      forecastHtml +
+      `
+    <div class="weather-forecast-day">
+    <div class="weather-forecast-date">${day}<div>
+    <div class="weather-forecast-icon">🌤️</div>
+    <div class="weather-forecast-temperatures">
+    <div class="weather-forecast-temperature">
+    <strong>15°</strong></div>
+     <div class="weather-forecast-temperature">9°</div>
+     </div>
+     </div>
+    `;
+  });
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = forecastHtml;
+}
+
 function formatDate(date) {
   let minutes = date.getMinutes();
   let hours = date.getHours();
@@ -55,3 +77,4 @@ let searchEngineElement = document.querySelector("#search-engine");
 searchEngineElement.addEventListener("submit", search);
 
 searchCity("Durban");
+displayForecast();
